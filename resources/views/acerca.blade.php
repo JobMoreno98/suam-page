a7ceb051ca97f9f78002a1fd1137f42536fb6ad1
Job Moreno <jobmoreno98@gmail.com>
2026-08-11 18:16:20 -0600

    se añaden las publicaciones

--- resources/views/acerca.blade.php
+++ resources/views/acerca.blade.php
@@ -42,7 +42,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5 5-5M12 15V3" />
                             </svg>
-                            <span>Descargar el dictamen de cración</span>
+                            <span>Descargar el dictamen de creación</span>
                         </a>
                     </div>
                 </div>
